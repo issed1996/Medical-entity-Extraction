f1e07f263c0fed82634bdd5c60d3588085fa033b
unknown <issed1996@gmail.com>
2023-08-12 17:41:27 +0200

    --

---
+++ config.py
@@ -0,0 +1,14 @@
+import transformers
+
+MAX_LEN = 512
+TRAIN_BATCH_SIZE = 4
+VALID_BATCH_SIZE = 2
+EPOCHS = 10
+BASE_MODEL_PATH = "dmis-lab/biobert-base-cased-v1.1"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "data/NCBI_corpus_training.txt"
+TESTING_FILE ="data/NCBI_corpus_testing.txt"
+TOKENIZER = transformers.BertTokenizer.from_pretrained(
+    BASE_MODEL_PATH,
+    do_lower_case=True
+)
