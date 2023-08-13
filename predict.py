ede340359ee16b2eecb829b8ad75b6b1ebb3a840
unknown <issed1996@gmail.com>
2023-08-13 21:32:58 +0200

    performing and to end prediction and argparse

--- predict.py
+++ predict.py
@@ -9,32 +9,26 @@ import engine
 from model import EntityModel
 
 
-if __name__ == "__main__":
+import argparse
+
+def predict_ner(sentence):#, model_name):
 
     meta_data = joblib.load("meta.bin")
-    #enc_pos = meta_data["enc_pos"]
     enc_tag = meta_data["enc_tag"]
-
-    #num_pos = len(list(enc_pos.classes_))
     num_tag = len(list(enc_tag.classes_))
 
-    sentence = """
-    abhishek is going to india
-    """
     tokenized_sentence = config.TOKENIZER.encode(sentence)
 
-    sentence = sentence.split()
-    print(sentence)
-    print(tokenized_sentence)
+    sentence = sentence.strip().split()
+    encodded_word_list=[config.TOKENIZER.encode(word,add_special_tokens=False) for word in sentence]
 
     test_dataset = dataset.EntityDataset(
-        texts=[sentence], 
-        #pos=[[0] * len(sentence)], 
+        texts=[sentence],  
         tags=[[0] * len(sentence)]
     )
 
-    device = torch.device("cuda")
-    model = EntityModel(num_tag=num_tag)#, num_pos=num_pos)
+    device = torch.device("cuda" )
+    model = EntityModel(num_tag=num_tag)
     model.load_state_dict(torch.load(config.MODEL_PATH))
     model.to(device)
 
@@ -44,8 +38,31 @@ if __name__ == "__main__":
             data[k] = v.to(device).unsqueeze(0)
         tag, _ = model(**data)
 
-        print(
-            enc_tag.inverse_transform(
-                tag.argmax(2).cpu().numpy().reshape(-1)
-            )[:len(tokenized_sentence)]
-        )
+    prediction= enc_tag.inverse_transform(
+                    tag.argmax(2).cpu().numpy().reshape(-1)
+                )[:len(tokenized_sentence)][1:-1]       
+
+
+    def most_common_element(lst):
+        return max(set(lst), key=lst.count)
+
+    final_prediction=[]
+    j=0
+    for encodded_word in encodded_word_list:
+      tag=most_common_element(list(prediction[j:j+len(encodded_word)]))
+      tag= tag[2:] if len(tag)>1 else tag
+      final_prediction.append(tag)
+      j = j+len(encodded_word)
+
+    print(f'input text_splited: {sentence}')
+    print(f'prediction du model: {final_prediction}')
+    
+
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Perform Named Entity Recognition using BioBERT model.")
+    parser.add_argument("--sentence", required=True, help="Input sentence for NER prediction.")
+    args = parser.parse_args()
+
+    predict_ner(args.sentence)
